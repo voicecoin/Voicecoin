6f2012069f1d5df55f12f79d38e5e16e2854f83a
Ubuntu <liferunon@sina.com>
2017-12-12 22:12:56 +0800

    add explorerv2

---
+++ explorerv2/tools/include.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+$dbconn = new mysqli("localhost", "root","zhaogang", "emcchain");
+// Check connection
+if ($dbconn->connect_error) {
+    die("Connection failed: " . $dbconn->connect_error);
+}
+
+$dbconn2 = new mysqli("localhost", "root","zhaogang", "emcchain");
+// Check connection
+if ($dbconn2->connect_error) {
+    die("Connection failed: " . $dbconn2->connect_error);
+}
+
+require_once '/var/www/tools/include/jsonRPCClient.php';
+$emercoin = new jsonRPCClient('http://zhao:gang@192.168.111.132:6662/');
+?>
